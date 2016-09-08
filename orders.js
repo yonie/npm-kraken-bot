947f40cece48639faf342f5950a1739aa2f61e46
Ronald Klarenbeek <yonie@yonie.org>
2016-09-08 20:20:16 +0200

    cleanup

--- orders.js
+++ orders.js
@@ -29,8 +29,7 @@ kraken.api('OpenOrders', null, function(error, data) {
 			orderTime = data.result.open[order].opentm;
 			orderType = data.result.open[order].descr.type;
 			// cancel order if it is too old
-			if (orderTime + maxAgeSeconds < currentTime || numOrders > maxOrders) {
-			//if (orderType == "buy" && (orderTime + maxAgeSeconds < currentTime || numOrders > maxOrders)) {
+			if ((orderTime + maxAgeSeconds < currentTime || numOrders > maxOrders)) {
 				log("Cancelling order #" + numOrders + " " + order + "...");
 				kraken.api('CancelOrder', { "txid" : order }, function (error, data) { 
 					if (error) log(error);

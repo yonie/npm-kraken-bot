7cae2fd12e502cf126511a120cc6e4aa279b960e
Ronald <yonie@yonie.org>
2017-01-31 23:31:36 +0100

    logging improvements

--- trades.js
+++ trades.js
@@ -5,8 +5,20 @@ var kraken = new KrakenClient(settings.krakenkey,settings.krakenpasscode);
 
 // get trade balance info
 kraken.api('TradesHistory', null, function(error, tradesHistoryData) {
-	if (!error) for (var trade in tradesHistoryData.result) {
-		console.log(tradesHistoryData.result[trade]);
+	if (!error) for (var trade in tradesHistoryData.result.trades) {
+		var logString = "";
+		var d = new Date(tradesHistoryData.result.trades[trade].time * 1000);
+
+		var datestring = ("0" + d.getDate()).slice(-2) + "-" + ("0"+(d.getMonth()+1)).slice(-2) + "-" + d.getFullYear() + " " + ("0" + d.getHours()).slice(-2) + ":" + ("0" + d.getMinutes()).slice(-2);
+
+		logString += datestring + " ";
+		logString += tradesHistoryData.result.trades[trade].pair + " "; 
+		logString += tradesHistoryData.result.trades[trade].type + " ";
+		logString += tradesHistoryData.result.trades[trade].ordertype + " ";
+		logString += parseFloat(tradesHistoryData.result.trades[trade].vol).toFixed(5) + " @ ";
+		logString += tradesHistoryData.result.trades[trade].price + " = ";
+		logString += tradesHistoryData.result.trades[trade].cost + " ";
+		console.log(logString);
 	}
 });
 

36515251b43a66a28d7a1668cbb86073a65ae3e0
Ronald <yonie@yonie.org>
2017-04-11 22:33:08 +0200

    refactor

--- log.js
+++ log.js
@@ -1,6 +1,10 @@
 // simple log helper
-module.exports = function(string, asset) {
+module.exports = function(string, tradepair) {
+
+	if (typeof tradepair === 'undefined') { tradepair = ""; } 
+
 	var d = new Date();
 	var datestring = ("0" + d.getDate()).slice(-2) + "-" + ("0"+(d.getMonth()+1)).slice(-2) + "-" + d.getFullYear() + " " + ("0" + d.getHours()).slice(-2) + ":" + ("0" + d.getMinutes()).slice(-2);
-	console.log(datestring + (asset!=null?" "+asset+currency:null) + " " + string);
+
+	console.log(datestring + ' ' + (tradepair != "" ? tradepair + ' ' : '') + string); 
 }

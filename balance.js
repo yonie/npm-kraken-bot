03e914b41364656f34561506e6866dd1351fb5d7
Ronald <yonie@yonie.org>
2017-09-30 23:03:32 +0200

    better deal with different assets

--- balance.js
+++ balance.js
@@ -13,11 +13,14 @@ kraken.api('TradeBalance', {"asset":"ZEUR"}, function(error, tradeBalanceData) {
 	kraken.api('Balance',null, function(error,balanceData) {
 		if (!error) {
 			// get ticker info to determine total value
-			kraken.api('Ticker', {"pair":"XETCZEUR,XETHZEUR,XLTCZEUR,XREPZEUR,XXBTZEUR,XXLMZEUR,XXMRZEUR,XZECZEUR"}, function(error,tickerData) {
-				for (var asset in balanceData.result) {
+//			kraken.api('Ticker', {"pair":"BCHEUR,DASHEUR,EOSEUR,GNOEUR,XETCZEUR,XETHZEUR,XLTCZEUR,XREPZEUR,XXBTZEUR,XXLMZEUR,XXMRZEUR,XXRPZEUR,XZECZEUR"}, function(error,tickerData) {
+			kraken.api('Ticker', {"pair":"BCHEUR,DASHEUR,XETCZEUR,XETHZEUR,XLTCZEUR,XREPZEUR,XXBTZEUR,XXMRZEUR,XXRPZEUR,XZECZEUR"}, function(error,tickerData) {
+				if (error) console.log(error);
+				else for (var asset in balanceData.result) {
 					if (balanceData.result[asset] && balanceData.result[asset]>=0.00001) {
 						var logString = asset+": " + parseFloat(balanceData.result[asset]).toFixed(5);
 						if (tickerData.result[asset+"ZEUR"]) logString = logString + " for " + tickerData.result[asset+"ZEUR"].c[0] + " = "+parseFloat(balanceData.result[asset]*tickerData.result[asset+"ZEUR"].c[0]).toFixed(2)+" ZEUR";
+						if (tickerData.result[asset+"EUR"]) logString = logString + " for " + tickerData.result[asset+"EUR"].c[0] + " = "+parseFloat(balanceData.result[asset]*tickerData.result[asset+"EUR"].c[0]).toFixed(2)+" EUR";
 						log(logString);
 					}
 				}

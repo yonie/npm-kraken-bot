79611f0d1b18cf96bfde5b6a09499093726bf770
Ronald <yonie@yonie.org>
2021-04-08 06:13:00 +0200

    use min trade amount from api, improved startup, hack for xdg api issue

--- server.js
+++ server.js
@@ -3,14 +3,11 @@
 var log = require("./log.js");
 log("initializing..")
 
-var createGraph = require("./createGraph.js");
-
 // get settings 
 var settings = require("./settings.js");
 var krakenkey = settings.krakenkey;
 var krakenpasscode = settings.krakenpasscode;
 var buyTolerance = settings.buyTolerance;
-var sellTolerance = settings.sellTolerance;
 var buyMoveLimit = settings.buyMoveLimit;
 var timer = settings.timer;
 var engineTick = 61 // seconds
@@ -20,7 +17,7 @@ const trading = true
 
 var ordersDirty = true
 
-const numHistory = 45
+const numHistory = 100
 
 var maxAgeSeconds = settings.maxAgeSeconds;
 
@@ -36,6 +33,7 @@ var kraken = new KrakenClient(krakenkey, krakenpasscode);
 var balance
 
 const http = require('http')
+const url = require('url')
 const port = 8000
 const server = http.createServer().listen(port)
 
@@ -83,24 +81,31 @@ server.on('request', (request, response) => {
         response.write("<a href=\"/trades\">trades</a><br/>")
         if (orders) response.write("<a href=\"/orders\">orders (" + orders.length + ")</a><br/>")
         response.write("<a href=\"/ticker\">ticker</a><br/>")
-        if (trades && trades.length > numHistory) {
+
+        var requestedPair = url.parse(request.url, true).query['pair']
+        if (requestedPair) response.write("<h3>" + requestedPair + ": " + (ticker ? ticker[requestedPair] : "") + "</h3>")
+
+        if (trades) {
             response.write("<p>latest trades:</p>")
             response.write("<ul>")
-            for (i = 0; i < numHistory; i++) {
-                if (!trades[i]) return
-                response.write("<li>")
-                response.write("" + new Date(trades[i]['time'] * 1000).toLocaleString())
-                response.write(" ")
-                response.write(trades[i]['type'])
-                response.write(" ")
-                response.write(trades[i]['vol'])
-                response.write(" ")
-                response.write(trades[i]['pair'])
-                response.write(" @ ")
-                response.write(trades[i]['price'])
-                response.write(" = ")
-                response.write(trades[i]['cost'])
-                response.write("</li>")
+            for (i = 0; i < Math.min(trades.length, numHistory); i++) {
+                if (!trades[i]) continue
+                if (!requestedPair || trades[i]['pair'] === requestedPair) {
+                    response.write("<li>")
+                    response.write("" + new Date(trades[i]['time'] * 1000).toLocaleString())
+                    response.write(" ")
+                    response.write(trades[i]['type'])
+                    response.write(" ")
+                    response.write(trades[i]['vol'])
+                    response.write(" <a href=\"?pair=" + trades[i]['pair'] + "\">")
+                    response.write(trades[i]['pair'])
+                    response.write("</a> ")
+                    response.write(" @ ")
+                    response.write(trades[i]['price'])
+                    response.write(" = ")
+                    response.write(trades[i]['cost'])
+                    response.write("</li>")
+                }
             }
             response.write("</ul>")
         }
@@ -119,6 +124,7 @@ var pairsExploded
 
 var altnames = []
 var pairPrecision = []
+var ordermin = []
 
 // determine pairs
 kraken.api('AssetPairs', null, function (error, pairdata) {
@@ -135,6 +141,7 @@ kraken.api('AssetPairs', null, function (error, pairdata) {
             pairs.push(assetpair)
             altnames[assetpair] = pairdata['result'][assetpair].altname
             pairPrecision[assetpair] = pairdata['result'][assetpair].pair_decimals
+            ordermin[assetpair] = pairdata['result'][assetpair].ordermin
         }
 
         // filter out what we want
@@ -142,7 +149,7 @@ kraken.api('AssetPairs', null, function (error, pairdata) {
             return pair.endsWith("EUR")
         })
 
-        // call cleanup tosee if we have all the asset pairs
+        // call cleanup to see if we have covered all the asset pairs
         pairs.forEach(function (pair) {
             trimPriceForAPI(pair, 1)
         })
@@ -152,8 +159,15 @@ kraken.api('AssetPairs', null, function (error, pairdata) {
         // get the exploded variant 
         pairsExploded = pairs.join()
 
-        if (trading) log("trading on pairs:" + pairsExploded)
+        if (trading) log("trading on pairs: " + pairsExploded)
         else log("NOT trading!")
+
+        // do initial requests
+        setTimeout(getTradeBalance, 1000)
+        setTimeout(getTradeHistory, 4000)
+        setTimeout(updateOpenOrders, 2000)
+        setTimeout(getTicker, 3000)
+
     }
 
 })
@@ -168,10 +182,12 @@ function shuffleArray(array) {
     }
 }
 
-var ticker = []
+var ticker
 
 // main routine
-setInterval(function () {
+setInterval(getTicker, 1000 * engineTick)
+
+function getTicker() {
 
     // get ticker info for all pairs
     kraken.api('Ticker', {
@@ -182,7 +198,7 @@ setInterval(function () {
             log("Error updating ticker: " + error)
         } else {
 
-            ticker = []
+            ticker = {}
 
             // loop through the pairs
             shuffleArray(pairs)
@@ -210,8 +226,8 @@ setInterval(function () {
                 var distancefromlow = Math.round((lasttrade - daylow) / (dayhi - daylow) * 100);
                 var move = Math.round(((dayhi - daylow) / dayhi) * 100);
 
-                // output fancy graph
-                ticker.push(pair + " " + createGraph(lasttrade, distancefromlow, daylow, dayhi, buyTolerance, sellTolerance) + " vol: " + tradevolume)
+                // build ticker
+                ticker[pair] = lasttrade + " (" + distancefromlow + "%/" + move + "%) lo:" + daylow + "/hi:" + dayhi + " vol(EUR): " + (tradevolume).toFixed(2)
 
                 // check if we have hard set to skip trading
                 if (!trading) return
@@ -227,10 +243,12 @@ setInterval(function () {
 
                     // we dont want to spend too much right now
                     // make sure we don't buy stuff below minimum trade volume
-                    if (wallet['ZEUR'].amount > balance * 0.8 && tradevolume > minvolume) {
+                    var shareOfWallet = 0.75
+                    if (wallet['ZEUR'] && wallet['ZEUR'].amount > balance * shareOfWallet && tradevolume > minvolume) {
 
                         var buyPrice = lasttrade * 0.995
                         var buyVolume = (fixedTradeEur / buyPrice).toFixed(8)
+                        if (ordermin[pair]) buyVolume = Math.max(buyVolume, ordermin[pair])
 
                         // quick hack to ensure proper input for API
                         buyPrice = trimPriceForAPI(pair, buyPrice)
@@ -238,7 +256,7 @@ setInterval(function () {
                         // if we have too much of one asset (including orders!), don't buy more
                         var openBuyOrderValue = sumOpenBuyOrderValue(pair)
                         var ownedAmount = (wallet && wallet[asset] ? wallet[asset]['value'] : null)
-                        
+
                         if ((buyVolume * buyPrice) + openBuyOrderValue + ownedAmount < 0.02 * balance) {
 
                             // buy stuff
@@ -246,7 +264,7 @@ setInterval(function () {
 
                             // TODO: dirty hack to make the orders again "dirty" otherwise we keep ordering until next update
                             ordersDirty = true
-                            setTimeout(updateOpenOrders,5000)
+                            setTimeout(updateOpenOrders, 5000)
 
                         }
                     }
@@ -270,7 +288,7 @@ setInterval(function () {
 
                         // TODO: dirty hack to make the orders again "dirty" otherwise we keep ordering until next update
                         ordersDirty = true
-                        setTimeout(updateOpenOrders,5000)
+                        setTimeout(updateOpenOrders, 5000)
 
                     }
                 }
@@ -279,7 +297,7 @@ setInterval(function () {
             })
         }
     })
-}, 1000 * engineTick)
+}
 
 // helper function to easily calculate the total amount of open order value for a given pair
 function sumOpenBuyOrderValue(pair) {
@@ -379,7 +397,9 @@ function updateOpenOrders() {
 var trades = []
 
 // get trade history info
-setInterval(function () {
+setInterval(getTradeHistory, 1000 * engineTick * 2)
+
+function getTradeHistory() {
     kraken.api('TradesHistory', null, function (error, tradesHistoryData) {
         if (error) {
             log("Error updating trades history: " + error)
@@ -387,16 +407,29 @@ setInterval(function () {
             trades = []
             for (var trade in tradesHistoryData.result.trades)
                 trades.push(tradesHistoryData.result.trades[trade])
+
+            kraken.api('TradesHistory', {
+                'ofs': 50
+            }, function (error, tradesHistoryData) {
+                if (error) {
+                    log("Error updating trades history: " + error)
+                } else {
+                    for (var trade in tradesHistoryData.result.trades)
+                        trades.push(tradesHistoryData.result.trades[trade])
+                }
+            })
         }
-    });
-}, 1000 * engineTick * 2)
+    })
+}
 
 var wallet = {}
 
 var balanceHistory = []
 
 // get trade balance info
-setInterval(function () {
+setInterval(getTradeBalance, 1000 * engineTick / 2)
+
+function getTradeBalance() {
     kraken.api('TradeBalance', {
         "asset": "ZEUR"
     }, function (error, tradeBalanceData) {
@@ -415,10 +448,15 @@ setInterval(function () {
                 if (error) {
                     log("Error getting balance: " + error)
                 } else {
+                    // FIXME: why is this here? we already init wallet
                     if (!wallet) wallet = {}
                     for (var asset in balanceData.result) {
                         var amount = parseFloat(balanceData.result[asset])
                         if (amount == 0) continue
+
+                        // FIXME: dirty hack due to messy kraken API 
+                        // ticker uses XDGEUR, wallet uses XXDG
+                        if (asset.indexOf("XXDG") > -1) asset = "XDG"
 
                         if (!wallet[asset]) wallet[asset] = {}
                         wallet[asset]['asset'] = asset
@@ -431,12 +469,14 @@ setInterval(function () {
             });
         }
     });
-}, 1000 * engineTick / 2)
+}
 
 // buy for a given price with built in timer with profit close order
 function buy(pair, volume, price, timer) {
 
     if (volume * price > 0) {
+
+        log("Adding order: buy " + volume + " " + pair + " @ " + price + "...")
 
         return kraken.api('AddOrder', {
             "pair": pair,
@@ -447,9 +487,9 @@ function buy(pair, volume, price, timer) {
             "expiretm": "+" + timer,
         }, function (error, buydata) {
             if (error) {
-                log("Error adding buy order: " + error)
+                log("Error adding buy order: " + error.message)
             } else if (buydata) {
-                log("[TRADE] " + buydata["result"]["descr"]["order"], pair);
+                log("[ORDER] " + buydata["result"]["descr"]["order"], pair);
             }
         });
     }
@@ -460,6 +500,8 @@ function sell(pair, volume, price, timer) {
 
     if (volume * price > 0) {
 
+        log("Adding order: sell " + volume + " " + pair + " @ " + price + "...")
+
         return kraken.api('AddOrder', {
             "pair": pair,
             "type": "sell",
@@ -469,9 +511,9 @@ function sell(pair, volume, price, timer) {
             "expiretm": "+" + timer,
         }, function (error, selldata) {
             if (error) {
-                log("Error adding sell order for pair " + pair + " " + volume + " " + price + "  : " + error)
+                log("Error adding sell order: " + error.message)
             } else if (selldata) {
-                log("[TRADE] " + selldata["result"]["descr"]["order"], pair);
+                log("[ORDER] " + selldata["result"]["descr"]["order"], pair);
             }
         });
     }

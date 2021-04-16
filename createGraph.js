babc53ac6863c5fa0370f2932ac69779c3d053a8
yonie <yonie@yonie.org>
2021-04-16 08:39:34 +0200

    code cleanup

--- createGraph.js
+++ createGraph.js
@@ -1,9 +1,9 @@
 // fancy graph to quickly see how asset is trading [bbb--*----ss]
-module.exports = function(lasttrade, distancefromlow, low, hi, buyTolerance, sellTolerance) {
+module.exports = function (lasttrade, distancefromlow, low, hi, buyTolerance, sellTolerance) {
 	var width = 10;
-	var move = Math.round(((hi-low)/hi)*100);
+	var move = Math.round(((hi - low) / hi) * 100);
 	var result = parseFloat(lasttrade).toFixed(5) + " | " + parseFloat(low).toFixed(5) + " [";
-	for (i=0;i<=width;i++) {
+	for (let i = 0; i <= width; i++) {
 		if ((i / width) * 100 >= distancefromlow && (i / width) * 100 < distancefromlow + (100 / width))
 			result = result + "*";
 		else if ((i / width) * 100 < buyTolerance)
@@ -14,6 +14,6 @@ module.exports = function(lasttrade, distancefromlow, low, hi, buyTolerance, sel
 			result = result + "-";
 	}
 
-	result = result + "] " + parseFloat(hi).toFixed(5) + " (" + distancefromlow + "%/"+move+"%)";
+	result = result + "] " + parseFloat(hi).toFixed(5) + " (" + distancefromlow + "%/" + move + "%)";
 	return result;
 }
--- log.js
+++ log.js
@@ -1,10 +1,12 @@
 // simple log helper
-module.exports = function(string, tradepair) {
+module.exports = function (string, tradepair) {
 
-	if (typeof tradepair === 'undefined') { tradepair = ""; } 
+	if (typeof tradepair === 'undefined') {
+		tradepair = "";
+	}
 
 	var d = new Date();
-	var datestring = ("0" + d.getDate()).slice(-2) + "-" + ("0"+(d.getMonth()+1)).slice(-2) + "-" + d.getFullYear() + " " + ("0" + d.getHours()).slice(-2) + ":" + ("0" + d.getMinutes()).slice(-2);
+	var datestring = ("0" + d.getDate()).slice(-2) + "-" + ("0" + (d.getMonth() + 1)).slice(-2) + "-" + d.getFullYear() + " " + ("0" + d.getHours()).slice(-2) + ":" + ("0" + d.getMinutes()).slice(-2);
 
-	console.log(datestring + ' ' + (tradepair != "" ? tradepair + ' ' : '') + string); 
+	console.log(datestring + ' ' + (tradepair != "" ? tradepair + ' ' : '') + string);
 }
